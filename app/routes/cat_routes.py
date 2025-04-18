1bfd18bc85d2eb7ab490f84c1dd789f3e15256a6
Malik Elmessiry <malik.messiry@gmail.com>
2025-04-18 20:01:41 +0200

    helper function

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from ..models.cat import cats
 
 cats_bp = Blueprint("cats_bp", __name__, url_prefix = "/cats")
@@ -16,3 +16,25 @@ def get_all_cats():
         ))
 
     return results_list
+
+@cats_bp.get("/<cat_id>")
+def get_one_cat(cat_id):
+    cat = validate_cat(cat_id)
+    return {
+        "id": cat.id,
+        "name": cat.name,
+        "color": cat.color,
+        "personality": cat.personality
+    }
+
+def validate_cat(cat_id):
+    try:
+        cat_id = int(cat_id)
+    except:
+        response = {"message": f"cat {cat_id} invalid"}
+        abort(make_response(response, 400))
+    for cat in cats:
+        if cat.id == cat_id:
+            return cat
+    response = {"message": f"cat {cat_id} not found"}
+    abort(make_response(response, 404))
